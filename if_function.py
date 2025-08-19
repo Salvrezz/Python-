b65b345170c0457f1b5ccdaf3c415184b3518356
Emmanuel Rock <rockchinonyerem@yahoo.com>
2025-08-19 08:13:36 +0100

    Add files via upload

---
+++ if_function.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+if 500 > 100 : 
+    print('Valid')
+
+
+# In[10]:
+
+
+if 500 < 100 : 
+    print('Valid')
+else:
+    print('invalid')
+
+
+# In[11]:
+
+
+if 500 < 100 or 100 < 200: 
+    print('Valid')
+else:
+    print('invalid')
+
+
+# In[16]:
+
+
+if 500 < 100: 
+    print('Valid')
+elif 200 < 100 :
+    print('unknown')
+elif 100 < 200 :
+    print('Maybe')
+
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+if 500 < 100: 
+    print('Valid')
+elif 200 < 100 :
+    print('unknown')
+elif 100 > 200 :
+    print('Maybe')
+else : 
+    print ('Invalid')
+
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+if 500 > 100: 
+    print('Valid')
+if 10 > 5 :
+    print('True')
+elif 200 < 100 :
+    print('unknown')
+elif 100 < 200 :
+    print('Maybe')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
