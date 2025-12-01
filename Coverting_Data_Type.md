797410bd6b60fae7ce03e97040e0e33aac019c8b
Izuazu Rock <rockchinonyerem@yahoo.com>
2025-12-01 16:14:03 +0100

    Tutorials
    This works contains all learning progress towards data analysis and data science using python.

---
+++ Coverting_Data_Type.md
@@ -0,0 +1,268 @@
+```python
+match = 25
+
+type(match)
+```
+
+
+
+
+    int
+
+
+
+
+```python
+day = '30'
+
+type(day)
+```
+
+
+
+
+    str
+
+
+
+
+```python
+day_conv = int(day)
+```
+
+
+```python
+type(day_conv)
+```
+
+
+
+
+    int
+
+
+
+
+```python
+print(match +day_conv)
+```
+
+    55
+    
+
+
+```python
+
+```
+
+
+```python
+age = (1,2,3,4,5)
+
+type(age)
+```
+
+
+
+
+    tuple
+
+
+
+
+```python
+age_conv = list(age)
+
+type(age_conv)
+```
+
+
+
+
+    list
+
+
+
+
+```python
+print(age_conv)
+```
+
+    [1, 2, 3, 4, 5]
+    
+
+
+```python
+
+```
+
+
+```python
+age_tuple = (1,2,3,4,5)
+
+type(age_tuple)
+```
+
+
+
+
+    tuple
+
+
+
+
+```python
+list(age_tuple)
+```
+
+
+
+
+    [1, 2, 3, 4, 5]
+
+
+
+
+```python
+type(list(age_tuple))
+```
+
+
+
+
+    list
+
+
+
+
+```python
+age_list = [1,1,2,2,3,3,4,3,5,5]
+
+type(age_list)
+```
+
+
+
+
+    list
+
+
+
+
+```python
+print(age_list)
+```
+
+    [1, 1, 2, 2, 3, 3, 4, 3, 5, 5]
+    
+
+
+```python
+set(age_list)
+```
+
+
+
+
+    {1, 2, 3, 4, 5}
+
+
+
+
+```python
+type(set(age_list))
+```
+
+
+
+
+    set
+
+
+
+
+```python
+
+```
+
+
+```python
+Player_mu = {'name':'Paul walker','age':30,'position':'cm'}
+
+type(Player)
+```
+
+
+
+
+    dict
+
+
+
+
+```python
+Player_mu.items()
+```
+
+
+
+
+    dict_items([('name', 'Paul walker'), ('age', 30), ('position', 'cm')])
+
+
+
+
+```python
+Player_mu.keys()
+```
+
+
+
+
+    dict_keys(['name', 'age', 'position'])
+
+
+
+
+```python
+Player_mu.values()
+```
+
+
+
+
+    dict_values(['Paul walker', 30, 'cm'])
+
+
+
+
+```python
+list(Player_mu.values())
+```
+
+
+
+
+    ['Paul walker', 30, 'cm']
+
+
+
+
+```python
+
+```
+
+
+```python
+ 
+```
+
+
+```python
+
+```
+
+
+```python
+
+```
---
+++ VARnDATAtype.py
@@ -0,0 +1,467 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+x = 25
+print(x)
+
+
+# In[ ]:
+
+
+y = 'Mike'
+print(y)
+
+
+# In[4]:
+
+
+x=y=z = ('obi', 'Mike', 'Chioma')
+print(x)
+print(y)
+print(z)
+
+
+# In[5]:
+
+
+x,y,z = ('obi', 'Mike', 'Chioma')
+print(x)
+print(y)
+print(z)
+
+
+# In[6]:
+
+
+Full_Name = ('John Don','Mike Law','Victory gate','Wash Limit')
+x,y,z,a = Full_Name
+print(x)
+print(y)
+print(z)
+print(a)
+
+
+# In[ ]:
+
+
+
+
+
+# In[13]:
+
+
+x=20 +20
+print(x)
+
+
+# In[ ]:
+
+
+
+
+
+# In[14]:
+
+
+ice_cream = 'chocolate','vanilla','orange'
+x,y,z = ice_cream 
+
+print(x)
+print(y)
+print(z)
+
+
+# In[ ]:
+
+
+
+
+
+# In[19]:
+
+
+ice_cream= ('chocolate','vanilla','orange')
+x=y=z = ice_cream
+
+print(x)
+
+
+# In[ ]:
+
+
+
+
+
+# In[20]:
+
+
+x = y = z = 'Jordan Road Heel'
+print(x)
+
+
+# In[ ]:
+
+
+
+
+
+# In[32]:
+
+
+#Getting second character alphabet from text
+a ='Ambient Nature'  
+print(a[1])
+
+
+# In[ ]:
+
+
+
+
+
+# In[33]:
+
+
+#Getting John from text 
+a ='John weds Mitchell'  
+print(a[:4])
+
+
+# In[35]:
+
+
+a +  a
+
+
+# In[ ]:
+
+
+
+
+
+# In[40]:
+
+
+('cookies','strawberry','chocolate')
+
+
+# In[51]:
+
+
+ice_cream = ['cookies','strawberry','chocolate']
+
+ice_cream.append('apple')
+
+ice_cream
+
+
+# In[52]:
+
+
+ice_cream = ['cookies','strawberry','chocolate']
+
+ice_cream[0] = 'orange'
+ice_cream
+
+
+# In[ ]:
+
+
+
+
+
+# In[54]:
+
+
+ice_cream = ['cookies','strawberry','chocolate']
+ice_cream[2] = 'orange'
+ice_cream
+
+
+# In[ ]:
+
+
+
+
+
+# In[57]:
+
+
+#tuple
+
+x = (1,2,3)
+type (x)
+
+
+# In[ ]:
+
+
+
+
+
+# In[58]:
+
+
+#b00lean
+
+1>5
+
+
+# In[59]:
+
+
+1==5
+
+
+# In[63]:
+
+
+x = 1<5
+
+type(x)
+
+
+# In[ ]:
+
+
+
+
+
+# In[65]:
+
+
+#List
+Full_Name = ['John Don','Mike Law','Victory gate','Wash Limit']
+Full_Name [1] = 'Above law'
+Full_Name
+
+
+# In[ ]:
+
+
+
+
+
+# In[73]:
+
+
+# sets
+
+daily_pints_log = {2,3,4,10,11}
+print(daily_pints_log)
+
+
+# In[74]:
+
+
+wife_daily_pints_log = {2,3,4,7,24,6}
+print(wife_daily_pints_log)
+
+
+# In[75]:
+
+
+print(daily_pints_log | wife_daily_pints_log)
+
+
+# In[76]:
+
+
+print(daily_pints_log & wife_daily_pints_log)
+
+
+# In[77]:
+
+
+print(daily_pints_log - wife_daily_pints_log)
+
+
+# In[ ]:
+
+
+
+
+
+# In[79]:
+
+
+#dictionaries
+
+dict_football = {'name':'Mich','position':'forward','club':['MU','Old Trafford']}
+type(dict_football)
+
+
+# In[ ]:
+
+
+
+
+
+# In[80]:
+
+
+#dictionaries
+
+dict_football = {'name':'Mich','position':'forward','club':['MU','Old Trafford']}
+print(dict_football)
+
+
+# In[81]:
+
+
+dict_football.keys()
+
+
+# In[ ]:
+
+
+
+
+
+# In[87]:
+
+
+10 == 10
+
+
+# In[88]:
+
+
+1!=10
+
+
+# In[89]:
+
+
+x = 'chips'
+y = 'mango'
+x == y
+
+
+# In[90]:
+
+
+x = 'chips'
+y = 'mango'
+x != y
+
+
+# In[ ]:
+
+
+
+
+
+# In[94]:
+
+
+full_name = ['ebuka obi', 'john will']
+x,y = full_name
+print(x)
+print(y)
+
+
+# In[ ]:
+
+
+
+
+
+# In[97]:
+
+
+(70 > 50) and (20 > 50)
+
+
+# In[98]:
+
+
+(70 > 50) or (20 > 50)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
